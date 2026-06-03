36a0ec7524f9568b19a1581b3c019c468c0cae98
AleksandraSieg <32368349+AleksandraSieg@users.noreply.github.com>
2026-06-03 15:05:56 +0200

    First commit

---
+++ src/main/java/com/example/joboffersjjrstart/JobOffersJjrStartApplication.java
@@ -0,0 +1,13 @@
+package com.example.joboffersjjrstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobOffersJjrStartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobOffersJjrStartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/joboffersjjrstart/JobOffersJjrStartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.joboffersjjrstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobOffersJjrStartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
